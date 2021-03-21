9539409fff441b29c5a6d7fca10ef0aa01efc053
Nortti <akhor@nordpage.ru>
2021-03-21 17:46:23 +0300

    Init

--- progrock/settings.py
+++ progrock/settings.py
@@ -54,7 +54,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'progrock.urls'
